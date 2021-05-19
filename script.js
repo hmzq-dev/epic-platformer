e33c69e9a0c54602716b496ebea2af726dd698a0
Jackbaklava <>
2021-05-19 18:42:29 +0000

    Added terrain generation and camera movement for when player moves

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+"use strict"
+
 //Variable Declarations
 let canvas = null;
-const windowMargin = 100;
+let score = 0;
 
-let score;
-let cloudImage;
-let groundImage;
+const groundHeight = 200;
+let groundGroup;
+let groundArray;
+let lastGround;
+
+let player;
 let playerImage;
+let ground;
+let groundImage;
+let cloudImage;
 let spikeImage;
 
 
@@ -19,16 +27,42 @@ function preload() {
 
 
 function setup() {
-  canvas = createCanvas(windowWidth -windowMargin, windowHeight -windowMargin);
+  noStroke();
+  canvas = createCanvas(800, 400);
   canvas.parent("screen");
+  
+  groundGroup = new Group();
+  ground = createSprite(0, 300, width, groundHeight);
+  groundGroup.add(ground);
+  
+  player = createSprite(100, 100, 30, 30);
+  player.velocity.x = 2;
+  player.velocity.y = 5;
 }
 
 
 function draw() {
   background("lightblue");
+  drawSprites();
+  generateTerrain();
+  
+  camera.position.x += player.velocity.x;
+  
+  player.overlap(groundGroup, () => player.velocity.y = 0);
+  
+  player.velocity.x += 0.05;
+  
+  //Update and display score
+  textSize(30);
+  score++;
+  text(`Score: ${score}`, camera.position.x, camera.position.y - 170);
 }
 
 
-function windowResized() {
-  resizeCanvas(windowWidth -windowMargin, windowHeight -windowMargin);
+function generateTerrain() {
+  groundArray = groundGroup.toArray();
+  lastGround = groundArray[groundArray.length - 1];
+  
+  ground = createSprite(lastGround.position.x + width, 300, width, groundHeight);
+  groundGroup.add(ground);
 }
