e7090438f42a56c963cd0cc9f9e293a7a1afce85
Jackbaklava <>
2021-05-27 19:55:31 +0000

    finished gam

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 "use strict";
 
 //Variable Declarations
-let speed = 3;
-let acceleration = 0.02;
+let accelerate = true;
+let speed = 1;
+let currentSpeed = speed;
+let acceleration = 0.007;
 const gravity = 5;
 let currentGravity = gravity;
 let playerIsJumping = false;
-let jumpDuration = 20; //Frames
+const jumpDuration = 20; //Frames
 let currentjumpPosition = -1;
 
 let spikesCanSpawn = false;
+let gameOver = false;
 let canvas = null;
 let score = 0;
+let highScore = 0;
+let tutorialEnded = false;
 
 const groundHeight = 200;
 let groundGroup;
@@ -27,11 +32,10 @@ let spikeGroup;
 let spike;
 let spikeImage;
 const spikeWidth = 30;
-const spikeHeight = 30;
+const spikeHeight = 50;
 
 //Canvas Methods
 function preload() {
-  cloudImage = loadImage("sprites/cloud.png");
   groundImage = loadImage("sprites/ground.png");
   playerImage = loadImage("sprites/player.png");
   spikeImage = loadImage("sprites/spike.png");
@@ -39,38 +43,43 @@ function preload() {
 
 
 function setup() {
-  noStroke();
   canvas = createCanvas(900, 500);
   canvas.parent("screen");
-  
-  groundGroup = new Group();
-  ground = createSprite(0, 400, width, groundHeight);
-  ground.setDefaultCollider();
-  groundGroup.add(ground);
-  
-  spikeGroup = new Group();
-  
-  player = createSprite(0, 250, 30, 30);
+  initGame();
 }
 
 
 function draw() {
-  background("lightblue");
-  drawSprites();
   textSize(30);
-  text("Press Space to Jump", 500, 200);
-  text("Avoid The Spikes", 1000, 200);
-  text("Good Luck! ( ͡° ͜ʖ ͡°)", 1500, 200)
-  
-  
-  if (player.position.x >= 1600) {
-    acceleration = 0.03;
-    spikesCanSpawn = true;
+  if (gameOver) {
+    gameOverScene();
+    return;
   }
   
-  speed += acceleration;
+  background("lightblue");
+  drawSprites();
+
+  if (!tutorialEnded) {
+    text("Press Space to Jump", 500, 200);
+    text("Avoid The Spikes", 1000, 200);
+    text("Good Luck! ( ͡° ͜ʖ ͡°)", 1500, 200);
+  }
   
-  player.position.x = player.position.x + speed;
+  if (player.position.x >= 2000) {
+    spikesCanSpawn = true;
+    tutorialEnded = true;
+  }
+  
+  //Weird accelerate boolean switching so that accelerates after every other frame
+  if (accelerate) {
+    currentSpeed += acceleration;
+    accelerate = false;
+  } else {
+    accelerate = true;
+  }
+  currentSpeed += acceleration;
+  
+  player.position.x = player.position.x + currentSpeed;
   player.position.y = player.position.y + currentGravity;
   camera.position.x = player.position.x + width/4;
   
@@ -88,6 +97,10 @@ function draw() {
     currentGravity = gravity;
   }
   
+  if (player.overlap(spikeGroup)) {
+    gameOver = true;
+  }
+  
   //Handle player jump initiation and end
   if (playerIsJumping) {
     currentjumpPosition += 1;
@@ -98,10 +111,11 @@ function draw() {
   }
   
   //Update and display score
-  score += Math.floor(speed);
-  text(`Score: ${score}`, camera.position.x - speed - 440, camera.position.y - 200);
-  text(Math.round(frameRate()), camera.position.x - speed - 200, camera.position.y - 200);
-  text(spikeGroup.toArray().length, camera.position.x - speed - 100, camera.position.y - 200);
+  score += Math.floor(currentSpeed);
+  text(`Score: ${score}`, camera.position.x - currentSpeed - 440, camera.position.y - 220);
+  text(`High Score: ${highScore}`, camera.position.x - currentSpeed - 440, camera.position.y - 180);
+
+  if (score >= highScore) highScore = score;
 }
 
 
@@ -110,20 +124,19 @@ function generateTerrain() {
   lastGround = groundArray[groundArray.length - 1];
   
   ground = createSprite(lastGround.position.x + width, 400, width, groundHeight);
+  ground.addImage(groundImage);
   ground.setDefaultCollider();
   groundGroup.add(ground);
   
   
   if (spikesCanSpawn) {
-    const r = randomInt(1, 3);
-    if (r === 1) {
-      const spikeX = randomInt(0, width+1);
-      spike = createSprite(ground.position.x + spikeX, 287, spikeWidth, spikeHeight);
-      
+    const r = randomInt(1, 6);
+    if (r > 2) {
+      const spikeX = randomInt(0, Math.round(width / 3));
+      spike = createSprite(ground.position.x + spikeX, 277, spikeWidth, spikeHeight);
+      spike.addImage(spikeImage);
       spike.setDefaultCollider();
       spikeGroup.add(spike);
-      
-      spike.debug = true;
     }
   }
   
@@ -140,6 +153,67 @@ function jump() {
   currentjumpPosition = 0;
 }
 
+
+function gameOverScene() {
+  background("white");
+  textStyle(BOLD);
+  textAlign(CENTER);
+  stroke("black");
+  
+  fill("red");
+  strokeWeight(3);
+  text("YOU DIED", camera.position.x, 100);
+  fill("black");
+  strokeWeight(1);
+  
+  fill("black");
+  text(`Score: ${score}`, camera.position.x, 200);
+  text(`High Score: ${highScore}`, camera.position.x, 250);
+  
+  fill("yellow");
+  strokeWeight(3);
+  text("Click Anywhere To Play Again", camera.position.x, 350);
+  
+  //Reset styles
+  textAlign(LEFT);
+  noStroke();
+}
+
+
+function initGame() {
+  groundGroup = new Group();
+  ground = createSprite(0, 400, width, groundHeight);
+  ground.addImage(groundImage);
+  ground.setDefaultCollider();
+  groundGroup.add(ground);
+  
+  spikeGroup = new Group();
+  
+  player = createSprite(0, 150, 30, 30);
+  player.addImage(playerImage);
+}
+
+
+function mousePressed() {
+  if (!gameOver) return;
+  
+  player.visible = false;
+  gameOver = false;
+  groundGroup.removeSprites();
+  spikeGroup.removeSprites();
+  
+  currentSpeed = speed + 4;
+  currentjumpPosition = -1;
+  playerIsJumping = false;
+  score = 0;
+  
+  ground = createSprite(0, 400, width, groundHeight);
+  ground.addImage(groundImage);
+  ground.setDefaultCollider();
+  groundGroup.add(ground);
+  player = createSprite(0, 150, 30, 30);
+  player.addImage(playerImage);
+}
 
 
 
